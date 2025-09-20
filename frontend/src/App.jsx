2901ffc72c86a75ce119ee7db916f7b4c3d2133d
HarshaVelpumadugu <velpumaduguharsha@gmail.com>
2025-09-20 09:51:47 +0530

    made some ui changes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -16,47 +16,43 @@ import EditAuctionItem from "./components/EditAuctionItem";
 import { Toaster } from "react-hot-toast";
 
 function App() {
-	return (
-		<AuthProvider>
-			<Router>
-			    <NavBar />
-				<Toaster/>
-				<div className="container mx-auto">
-					<Routes>
-						<Route path="/" element={<Home />} />
-						<Route path="/signup" element={<Signup />} />
-						<Route path="/login" element={<Login />} />
-						<Route path="/logout" element={<Logout />} />
-						<Route path="/auctions" element={<AuctionList />} />
-						<Route
-							path="/profile"
-							element={
-								<ProtectedRoute component={Profile} />}
-						/>
-						<Route
-							path="/auction/:id"
-							element={<ProtectedRoute component={AuctionItem} />}
-						/>
-						<Route
-							path="/auction/create"
-							element={
-								<ProtectedRoute component={CreateAuctionItem} />}
-						/>
-						<Route
-							path="/auction/edit/:id"
-							element={
-								<ProtectedRoute component={EditAuctionItem} />}
-						/>
-						<Route
-							path="/auction/bid/:id"
-							element={
-								<ProtectedRoute component={BidForm} />}
-						/>
-					</Routes>
-				</div>
-			</Router>
-		</AuthProvider>
-	);
+  return (
+    <AuthProvider>
+      <Router>
+        <NavBar />
+        <Toaster />
+        <div className="max-w-full mx-auto">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/signup" element={<Signup />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/logout" element={<Logout />} />
+            <Route path="/auctions" element={<AuctionList />} />
+            <Route
+              path="/profile"
+              element={<ProtectedRoute component={Profile} />}
+            />
+            <Route
+              path="/auction/:id"
+              element={<ProtectedRoute component={AuctionItem} />}
+            />
+            <Route
+              path="/auction/create"
+              element={<ProtectedRoute component={CreateAuctionItem} />}
+            />
+            <Route
+              path="/auction/edit/:id"
+              element={<ProtectedRoute component={EditAuctionItem} />}
+            />
+            <Route
+              path="/auction/bid/:id"
+              element={<ProtectedRoute component={BidForm} />}
+            />
+          </Routes>
+        </div>
+      </Router>
+    </AuthProvider>
+  );
 }
 
 export default App;
